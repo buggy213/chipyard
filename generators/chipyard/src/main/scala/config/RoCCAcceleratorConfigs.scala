aeb0b722ce99968823c2e7dfa662159d5c929f69
Hasan Genc <hngenc@berkeley.edu>
2024-01-13 14:18:54 -0800

    add einstein configs

--- generators/chipyard/src/main/scala/config/RoCCAcceleratorConfigs.scala
+++ generators/chipyard/src/main/scala/config/RoCCAcceleratorConfigs.scala
@@ -2,6 +2,7 @@ package chipyard
 
 import org.chipsalliance.cde.config.{Config}
 import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
+import freechips.rocketchip.subsystem.WithoutTLMonitors
 
 // ------------------------------
 // Configs with RoCC Accelerators
@@ -55,4 +56,95 @@ class HwachaLargeBoomConfig extends Config(
   new hwacha.DefaultHwachaConfig ++                              // use Hwacha vector accelerator
   new boom.common.WithNLargeBooms(1) ++
   new chipyard.config.WithSystemBusWidth(128) ++
+  new chipyard.config.AbstractConfig)
+
+class EinsteinRocketConfig extends Config(
+  new einstein.DefaultEinsteinConfig ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class SmallEinsteinRocketConfig extends Config(
+  new einstein.LargeDenseEinsteinConfig(size=4) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MidEinsteinRocketConfig extends Config(
+  new einstein.LargeDenseEinsteinConfig(size=8) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class LargeEinsteinRocketConfig extends Config(
+  new einstein.LargeDenseEinsteinConfig(size=16) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class SparseDenseEinsteinRocketConfig extends Config(
+  new einstein.SparseDenseEinsteinConfig ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class SmallSparseDenseEinsteinRocketConfig extends Config(
+  new einstein.SparseDenseEinsteinConfig(size = 4) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MidSparseDenseEinsteinRocketConfig extends Config(
+  new einstein.SparseDenseEinsteinConfig(size = 8) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class LargeSparseDenseEinsteinRocketConfig extends Config(
+  new einstein.SparseDenseEinsteinConfig(size = 16) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class LoadBalancedSparseDenseEinsteinRocketConfig extends Config(
+  new einstein.SparseDenseEinsteinConfig(isLoadBalanced = true) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class OSSparseDenseEinsteinRocketConfig extends Config(
+  new einstein.SparseDenseEinsteinConfig(outputStationary = true) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class OuterSpaceMatmulEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 2, hasMatmul = true, hasMerger = false) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class SmallOuterSpaceMatmulEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 4, hasMatmul = true, hasMerger = false) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MidOuterSpaceMatmulEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 8, hasMatmul = true, hasMerger = false) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class LargeOuterSpaceMatmulEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 16, hasMatmul = true, hasMerger = false) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class OuterSpaceMergerEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 2, hasMatmul = false, hasMerger = true) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new WithoutTLMonitors ++
+  new chipyard.config.AbstractConfig)
+
+class SmallOuterSpaceMergerEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 4, hasMatmul = false, hasMerger = true) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MidOuterSpaceMergerEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 8, hasMatmul = false, hasMerger = true) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class LargeOuterSpaceMergerEinsteinRocketConfig extends Config(
+  new einstein.OuterSpaceEinsteinConfig(size = 16, hasMatmul = false, hasMerger = true) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
   new chipyard.config.AbstractConfig)
